4ecb4733390e7ed55dd356c77743135648c13277
AlexKuryanov <lexxus07@yandex.ru>
2023-01-11 00:47:25 +0100

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.part = Math.round((this.max + this.min) / 2);
+    return this.part;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.part;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.part;
+  }
 }
 
 module.exports = GuessingGame;
